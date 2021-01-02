3b401f9218e4423d21847a7c026ac02b3d4a8c3b
Rafael Julio <hello.rjulio@gmail.com>
2021-01-02 19:50:55 -0300

    chore: add ESLint to project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 import { AppProps } from 'next/app';
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }: AppProps): unknown {
+  return <Component {...pageProps} />;
 }
 
-export default MyApp;
+export default App;
