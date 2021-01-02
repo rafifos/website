2bab05654209db56f664dd5e27881525c6ad5b29
Rafael Julio <hello.rjulio@gmail.com>
2021-01-02 20:03:45 -0300

    chore: add Prettier to the project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
-import { AppProps } from 'next/app';
-import '../styles/globals.css';
+import { AppProps } from 'next/app'
+import '../styles/globals.css'
 
 function App({ Component, pageProps }: AppProps): unknown {
-  return <Component {...pageProps} />;
+  return <Component {...pageProps} />
 }
 
-export default App;
+export default App
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,12 +6,12 @@ export default function Home(): JSX.Element {
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href='https://nextjs.org'>Next.js!</a>
         </h1>
 
         <p className={styles.description}>
@@ -20,18 +20,18 @@ export default function Home(): JSX.Element {
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
+          <a href='https://nextjs.org/docs' className={styles.card}>
             <h3>Documentation &rarr;</h3>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
+          <a href='https://nextjs.org/learn' className={styles.card}>
             <h3>Learn &rarr;</h3>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
           <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
+            href='https://github.com/vercel/next.js/tree/master/examples'
             className={styles.card}
           >
             <h3>Examples &rarr;</h3>
@@ -39,7 +39,7 @@ export default function Home(): JSX.Element {
           </a>
 
           <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href='https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
             className={styles.card}
           >
             <h3>Deploy &rarr;</h3>
@@ -52,12 +52,12 @@ export default function Home(): JSX.Element {
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          <img src='/vercel.svg' alt='Vercel Logo' className={styles.logo} />
         </a>
       </footer>
     </div>
