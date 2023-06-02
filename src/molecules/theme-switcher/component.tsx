4fbab4d2612bb90d14d1b26345eee1676c5d13d0
Rafael Julio <development@rafifos.dev>
2023-06-02 23:24:44 +0000

    fix(SwitchTheme): fix responsivity and background color
    Signed-off-by: Rafael Julio <development@rafifos.dev>

--- src/molecules/theme-switcher/component.tsx
+++ src/molecules/theme-switcher/component.tsx
@@ -11,10 +11,12 @@ function SwitchTheme() {
   return (
     <details className="dropdown-end dropdown z-40">
       <summary className="btn-ghost btn m-1">
-        <MdOutlinePalette size={24} /> Tema <MdExpandMore />
+        <MdOutlinePalette size={24} />
+        <span className="hidden sm:inline-block">Tema</span>
+        <MdExpandMore />
       </summary>
 
-      <ul className="max-w-52 dropdown-content menu rounded-box max-h-[70vh] flex-nowrap overflow-auto p-2 shadow-lg">
+      <ul className="max-w-52 dropdown-content menu rounded-box max-h-[70vh] flex-nowrap overflow-auto bg-base-100 p-2 shadow-lg">
         {themes.map((theme, key) => (
           <li key={key} onClick={() => setTheme(theme as string)}>
             <a>{theme}</a>
