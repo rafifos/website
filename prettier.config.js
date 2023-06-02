0e5744f601af137c4bb796731adc7adece47687a
Rafael Julio <development@rafifos.dev>
2023-06-02 02:31:21 +0000

    chore(prettier): add tailwindcss plugin
    Signed-off-by: Rafael Julio <development@rafifos.dev>

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+const tailwindcss = require("prettier-plugin-tailwindcss");
+
+module.exports = {
+  printWidth: 100,
+  proseWrap: "always",
+  quoteProps: "consistent",
+  plugins: [tailwindcss],
+};
