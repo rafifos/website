542c0830ec7cc7ad7a201a93e024cf9d19ed739a
Rafael Julio <development@rafifos.dev>
2023-06-22 20:29:17 +0000

    feat(404): create 404 page
    Signed-off-by: Rafael Julio <development@rafifos.dev>

---
+++ src/app/not-found.tsx
@@ -0,0 +1,13 @@
+export default function NotFound() {
+  return (
+    <main className="m-auto flex max-w-7xl flex-col gap-8 p-4 text-center">
+      <h1 className="font-mono text-9xl font-extrabold">404</h1>
+      <p>
+        Oops! Parece que nossos gatos fizeram uma bagunça nessa página. O Sputnik, a Hayley
+        Williams, e o Sylvester, estão aprontando das suas. Enquanto tentamos colocar tudo em ordem,
+        que tal dar um ronronada de volta à página inicial? Eles prometeram se comportar... pelo
+        menos por enquanto!
+      </p>
+    </main>
+  );
+}
