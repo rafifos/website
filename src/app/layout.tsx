ae40bc474e2f638a2e9595c3ab0191c2eb85c1ba
Rafael Julio <development@rafifos.dev>
2023-09-26 00:26:05 +0000

    refactor(fonts) move to assets folder
    Signed-off-by: Rafael Julio <development@rafifos.dev>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,9 +6,9 @@ import { Analytics } from "@vercel/analytics/react";
 import clsx from "clsx";
 
 import Providers from "@/app/providers";
+import { inter, iosevka } from "@/assets/fonts";
 import { Footer } from "@/organisms/footer";
 import { Header } from "@/organisms/header";
-import { inter, iosevka } from "@/styles/fonts";
 
 const metadata: Metadata = {
   title: "Rafael Julio | Desenvolvedor Front-End",
