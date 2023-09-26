0a185ad22d1dec84d48ae2e9a079207bd593f651
Rafael Julio <development@rafifos.dev>
2023-09-26 00:23:56 +0000

    refactor(Hero): use static import for profile picture
    Signed-off-by: Rafael Julio <development@rafifos.dev>

--- src/organisms/hero/component.tsx
+++ src/organisms/hero/component.tsx
@@ -2,16 +2,19 @@ import Image from "next/image";
 
 import { Contact, ContactLinks } from "@/atoms/contact";
 
+import profilePic from "./assets/profile-picture.jpg";
+
 function Hero() {
   return (
     <section className="hero">
       <div className="hero-content flex-col gap-8 p-0 lg:flex-row-reverse">
         <Image
-          src="/assets/profile-picture.jpg"
+          src={profilePic}
           alt="Foto de Perfil do Rafael"
           className="rounded-lg shadow-2xl"
           width={225}
           height={300}
+          placeholder="blur"
         />
 
         <div className="flex flex-col gap-2">
