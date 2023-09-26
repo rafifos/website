f4576f30aeffeec3cd18d802b00f3cb01feb7872
Rafael Julio <development@rafifos.dev>
2023-09-26 00:16:02 +0000

    feat: add million
    Signed-off-by: Rafael Julio <development@rafifos.dev>

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
+const million = require("million/compiler");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = nextConfig;
+/** @type {{
+  optimize?: boolean;
+  server?: boolean;
+  mode?: 'react' | 'preact' | 'react-server' | 'preact-server' | 'vdom';
+  mute?: boolean | 'info';
+  hmr?: boolean;
+  auto?: boolean | {
+    threshold?: number;
+    rsc?: boolean;
+    skip?: (string | RegExp)[];
+  };
+  _file?: string;
+}} */
+const millionConfig = {
+  auto: { rsc: true },
+};
+
+module.exports = million.next(nextConfig, millionConfig);
